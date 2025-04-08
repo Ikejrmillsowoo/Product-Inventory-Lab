a89b7570f7658423c3a7e6de6b7819ce79c92882
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-08 16:32:02 -0400

    constructor test completed in whiskey

--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -4,8 +4,68 @@ public class Whiskey {
     private int id;
     private String name;
     private String brand;
-//    private String sport;
     private int size;
     private int qty;
     private float price;
+
+public Whiskey(){
+
+}
+
+    public Whiskey(int id, String name, String brand, int size, int qty, float price) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.size = size;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public int getQty() {
+        return this.qty;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
